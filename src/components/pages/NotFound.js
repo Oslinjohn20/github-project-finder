b4bd18b86c619a85245f3e9f74c51ebd8aa88669
Oslinjohn20 <ossiekcjohnson@gmail.com>
2020-06-11 18:27:10 +0200

    pre polished product

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notfound = () => {
+    return (
+        <div>
+            <h1>Not Found</h1>
+            <p className='lead'>The page that your are looking for does not exist...</p>
+        </div>
+    )
+}
+export default Notfound;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import UserItem from "./UserItem";
-import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types'
+import Spinner from "../layout/Spinner";
+import GithubContext from "../../context/github/githubContext";
 
+const Users = () => {
+	const githubContext = useContext(GithubContext);
 
-const Users = ({ users, loading }) => {
+	const { users, loading } = githubContext;
 	if (loading) {
-		return <Spinner/>
+		return <Spinner />;
 	} else {
 		return (
 			<div style={userStyle}>
@@ -16,13 +18,7 @@ const Users = ({ users, loading }) => {
 			</div>
 		);
 	}
-	
 };
-
-Users.propTypes = {
-	users: PropTypes.array.isRequired,
-	loading: PropTypes.bool.isRequired, 
-}
 
 const userStyle = {
 	display: "grid",
